aa7bc35d5d6af13002f5b5059f7739e42f78698b
Eliezer <68651041+softonesoporte3@users.noreply.github.com>
2022-10-29 09:59:12 -0400

    added color change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,166 +1,165 @@
-import { useEffect, useState } from "react";
-import Navbar from "./Navbar";
-import { getDatabase, ref, set, onValue } from "firebase/database";
-import { randomId, getDateFormated } from "./utils";
-import NotesList from "./NotesList";
-import { Fab } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
+import { randomId, getDateFormated } from './utils';
+import NotesList from './NotesList';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function HomeScreen() {
-  const [notes, setNotes] = useState([]);
-  const [fullData, setFullData] = useState([]);
+	const [notes, setNotes] = useState([]);
+	const [fullData, setFullData] = useState([]);
 
-  //Sort function
-  const sortByDate = (data) => {
-    const tempdata = [];
-    Object.entries(data).forEach((obj) => {
-      tempdata.push(obj);
-    });
+	//Sort function
+	const sortByDate = (data) => {
+		const tempdata = [];
+		Object.entries(data).forEach((obj) => {
+			tempdata.push(obj);
+		});
 
-    tempdata.sort((a, b) => {
-      return b[1]["date-info"]["orderBy"] - a[1]["date-info"]["orderBy"];
-    });
+		tempdata.sort((a, b) => {
+			return b[1]['date-info']['orderBy'] - a[1]['date-info']['orderBy'];
+		});
 
-    return tempdata;
-  };
+		return tempdata;
+	};
 
-  //Search function
-  const search = (text) => {
-    let temparr = [];
+	//Search function
+	const search = (text) => {
+		let temparr = [];
 
-    if (text !== "") {
-      refreshData();
-      fullData.forEach((elem) => {
-        if (elem[1]["note"].includes(text) || elem[1]["title"].includes(text)) {
-          console.log(elem);
-          temparr.push(elem);
-        }
-      });
-      setNotes(temparr);
-    } else {
-      refreshData();
-    }
-  };
+		if (text !== '') {
+			refreshData();
+			fullData.forEach((elem) => {
+				if (elem[1]['note'].includes(text) || elem[1]['title'].includes(text)) {
+					console.log(elem);
+					temparr.push(elem);
+				}
+			});
+			setNotes(temparr);
+		} else {
+			refreshData();
+		}
+	};
 
-  //If not logged then move to root
-  useEffect(() => {
-    if (localStorage.getItem("email") == null) {
-      window.location.pathname = "";
-    }
-  });
+	//If not logged then move to root
+	useEffect(() => {
+		if (localStorage.getItem('email') == null) {
+			window.location.pathname = '';
+		}
+	});
 
-  //Create new note
-  const newNote = () => {
-    const db = getDatabase();
-    set(ref(db, `users/${localStorage.getItem("uid")}/notes/${randomId()}`), {
-      title: "",
-      note: "",
-      color: "#395b64",
-      "date-info": {
-        created: getDateFormated(),
-        modified: getDateFormated(),
-        orderBy: Date.now(),
-      },
-    }).then(() => {
-      refreshData();
-    });
-  };
+	//Create new note
+	const newNote = () => {
+		const db = getDatabase();
+		set(ref(db, `users/${localStorage.getItem('uid')}/notes/${randomId()}`), {
+			title: '',
+			note: '',
+			color: '#395b64',
+			'date-info': {
+				created: getDateFormated(),
+				modified: getDateFormated(),
+				orderBy: Date.now(),
+			},
+		}).then(() => {
+			refreshData();
+		});
+	};
 
-  //Get current page title
-  const getTitle = () => {
-    switch (window.location.pathname) {
-      case "/home":
-        return "Home";
-      case "/":
-        return "Notes App";
-      default:
-        return "jeje";
-    }
-  };
+	//Get current page title
+	const getTitle = () => {
+		switch (window.location.pathname) {
+			case '/home':
+				return 'Home';
+			case '/':
+				return 'Notes App';
+			default:
+				return 'jeje';
+		}
+	};
 
-  //Refresh data
-  const refreshData = () => {
-    const db = getDatabase();
-    onValue(
-      ref(db, `users/${localStorage.getItem("uid")}/notes`),
-      (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          setFullData(sortByDate(data));
-          setNotes(sortByDate(data));
-        }
-      }
-    );
-  };
+	//Refresh data
+	const refreshData = () => {
+		const db = getDatabase();
+		onValue(
+			ref(db, `users/${localStorage.getItem('uid')}/notes`),
+			(snapshot) => {
+				const data = snapshot.val();
+				if (data) {
+					setFullData(sortByDate(data));
+					setNotes(sortByDate(data));
+				}
+			},
+		);
+	};
 
-  //Initialize and listen to changes in page
-  useEffect(() => {
-    const db = getDatabase();
+	//Initialize and listen to changes in page
+	useEffect(() => {
+		const db = getDatabase();
 
-    onValue(
-      ref(db, `users/${localStorage.getItem("uid")}/notes`),
-      (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          setFullData(sortByDate(data));
-          setNotes(sortByDate(data));
-        }
-      }
-    );
+		onValue(
+			ref(db, `users/${localStorage.getItem('uid')}/notes`),
+			(snapshot) => {
+				const data = snapshot.val();
+				if (data) {
+					setFullData(sortByDate(data));
+					setNotes(sortByDate(data));
+				}
+			},
+		);
 
-    window.onscroll = function () {
-      myFunction();
-    };
+		window.onscroll = function () {
+			myFunction();
+		};
 
-    let navbar = document.getElementById("navbar");
-    let sticky = navbar.offsetTop;
+		let navbar = document.getElementById('navbar');
+		let sticky = navbar.offsetTop;
 
-    function myFunction() {
-      if (window.pageYOffset >= sticky && window.pageYOffset > 30) {
-        navbar.classList.add("sticky");
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-  }, []);
+		function myFunction() {
+			if (window.pageYOffset >= sticky && window.pageYOffset > 30) {
+				navbar.classList.add('sticky');
+			} else {
+				navbar.classList.remove('sticky');
+			}
+		}
+	}, []);
 
-  const test = () => {
-    const db = getDatabase();
-    set(ref(db, `users/${localStorage.getItem("uid")}/notes/${randomId()}`), {
-      title: "",
-      note: "",
-      color: "#395b64",
-      "date-info": {
-        created: "a",
-        modified: "a",
-        orderBy: 1,
-      },
-    });
-  };
+	const test = () => {
+		const db = getDatabase();
+		set(ref(db, `users/${localStorage.getItem('uid')}/notes/${randomId()}`), {
+			title: '',
+			note: '',
+			color: '#395b64',
+			'date-info': {
+				created: 'a',
+				modified: 'a',
+				orderBy: 1,
+			},
+		});
+	};
 
-  return (
-    <div className="main-container">
-      <Navbar title={getTitle()}></Navbar>
-      <button onClick={test}>asd</button>
-      <div className="searchbar-container">
-        <SearchIcon className="search-icon"></SearchIcon>
-        <input
-          className="search-input"
-          type="text"
-          onChange={(e) => {
-            search(e.target.value);
-          }}
-        />
-      </div>
-      <div>
-        <NotesList data={notes}></NotesList>
-      </div>
-      <div className="fab-container">
-        <Fab color="primary" aria-label="add" onClick={newNote}>
-          <AddIcon />
-        </Fab>
-      </div>
-    </div>
-  );
+	return (
+		<div className='main-container'>
+			<Navbar title={getTitle()}></Navbar>
+			<div className='searchbar-container'>
+				<SearchIcon className='search-icon'></SearchIcon>
+				<input
+					className='search-input'
+					type='text'
+					onChange={(e) => {
+						search(e.target.value);
+					}}
+				/>
+			</div>
+			<div>
+				<NotesList data={notes}></NotesList>
+			</div>
+			<div className='fab-container'>
+				<Fab color='primary' aria-label='add' onClick={newNote}>
+					<AddIcon />
+				</Fab>
+			</div>
+		</div>
+	);
 }
--- src/components/Material-Components/NoteButtons.js
+++ src/components/Material-Components/NoteButtons.js
@@ -1,35 +1,101 @@
-import { getDatabase, ref, set, onValue, remove } from "firebase/database";
-import DeleteIcon from "@mui/icons-material/Delete";
-import IconButton from "@mui/material/IconButton";
-import PaletteIcon from "@mui/icons-material/Palette";
+import { getDatabase, ref, set, get, child, remove } from 'firebase/database';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import PaletteIcon from '@mui/icons-material/Palette';
+import { useState } from 'react';
 
 export default function CardButtons(props) {
-  const handleDelete = () => {
-    const db = getDatabase();
-    remove(ref(db, `users/${localStorage.getItem("uid")}/notes/${props.nid}`));
-  };
+	const [data, setData] = useState([]);
 
-  const handleChangeColor = () => {
-    console.log("color");
-  };
+	const handleDelete = () => {
+		const db = getDatabase();
+		remove(ref(db, `users/${localStorage.getItem('uid')}/notes/${props.nid}`));
+	};
 
-  return (
-    <div>
-      <IconButton
-        aria-label="delete"
-        onClick={handleDelete}
-        style={{ color: "white" }}
-      >
-        <DeleteIcon />
-      </IconButton>
+	const color = ['#395b64', 'red', 'blue', 'white'];
 
-      <IconButton
-        aria-label="color"
-        onClick={handleChangeColor}
-        style={{ color: "white" }}
-      >
-        <PaletteIcon />
-      </IconButton>
-    </div>
-  );
+	const writeData = (data) => {
+		const db = getDatabase();
+		let index = color.indexOf(props.color);
+		let nextColor = 0;
+
+		if (index + 1 !== color.length) {
+			nextColor = index + 1;
+			console.log(`color lenght: ${color.length}`);
+			console.log(`index: ${index}`);
+			console.log(`nextColor: ${nextColor}`);
+		}
+
+		set(ref(db, `users/${localStorage.getItem('uid')}/notes/${props.nid}`), {
+			title: data['title'],
+			note: data['note'],
+			color: color[nextColor],
+			'date-info': {
+				created: data['date-info']['created'],
+				modified: data['date-info']['modified'],
+				orderBy: data['date-info']['orderBy'],
+			},
+		});
+	};
+
+	const handleChangeColor = () => {
+		const db = getDatabase();
+
+		get(
+			child(ref(db), `users/${localStorage.getItem('uid')}/notes/${props.nid}`),
+		)
+			.then((snapshot) => {
+				if (snapshot.exists()) {
+					console.log(snapshot.val());
+
+					writeData(snapshot.val());
+				} else {
+					console.log(props.nid);
+					console.log('No data available');
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+
+		/* if (data[0] !== undefined) {
+			let index = color.indexOf(props.color);
+			let nextColor = 0;
+
+			if (index <= color.length) {
+				nextColor = index + 1;
+			}
+
+			set(ref(db, `users/${localStorage.getItem('uid')}/notes/${props.nid}`), {
+				title: data['title'],
+				note: data['note'],
+				color: color[nextColor],
+				'date-info': {
+					created: data['date-info']['created'],
+					modified: data['date-info']['modified'],
+					orderBy: data['date-info']['orderBy'],
+				},
+			});
+		} */
+	};
+
+	return (
+		<div>
+			<IconButton
+				aria-label='delete'
+				onClick={handleDelete}
+				style={{ color: 'white' }}
+			>
+				<DeleteIcon />
+			</IconButton>
+
+			<IconButton
+				aria-label='color'
+				onClick={handleChangeColor}
+				style={{ color: 'white' }}
+			>
+				<PaletteIcon />
+			</IconButton>
+		</div>
+	);
 }
